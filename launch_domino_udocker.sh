d0acde823bf5360434273f8d71d5a780b6fd7693
laramaktub <lara.cern@gmail.com>
2017-09-26 16:40:03 +0200

    Launcher to run without root privileges

---
+++ launch_domino_udocker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Prepare the environment to run with graphic interface
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+exectype=$1
+
+if [ $exectype = "gui" ];
+then
+	echo "Running docker in GUI mode"
+
+	echo "Please enter the path to your input files:" 
+
+	echo ""
+	read inputpath
+	echo "Please enter the path to your outputfolder:" 
+
+	echo ""
+	read outputpath
+
+	udocker run   -v $inputpath:/input -v $outputpath:/output -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH laramaktub/docker-domino DOMINO
+
+else
+
+	if [ $exectype = "commandline" ];
+
+        	then
+		echo "Running docker in command line mode"
+		udocker run   -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH laramaktub/docker-domino /bin/bash
+
+	else
+
+	echo "Wrong input parameter. Correct parameters are: commandline or gui"
+
+	fi
+
+fi
